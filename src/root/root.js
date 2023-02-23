3f4ee3cfcf9c5f2a1a37594fdef7f4aca7f7cfbc
CIVILLIAN98 <civillian1998@gmail.com>
2023-02-23 14:24:43 +0500

    template improved

--- src/root/root.js
+++ src/root/root.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import { Navbar } from "../components/navbar";
+import { Home } from "../components/navbar/home";
 
 class Root extends Component {
   render() {
-    return <></>;
+    return (
+      <>
+        <Navbar />
+        <Home />
+      </>
+    );
   }
 }
 export default Root;
